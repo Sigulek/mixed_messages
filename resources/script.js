8d6b50a6991ea4f6dbcd40db0d90c35bd011b5dd
Denis Sigulin <118192424+Sigulek@users.noreply.github.com>
2022-12-07 00:21:50 +0400

    1st final version

--- resources/script.js
+++ resources/script.js
@@ -9,10 +9,54 @@ const phrases = ['Be yourself; everyone else is already taken.',
 ];
 const zodiacs = ['Ram', 'Bull', 'Twins', 'Crab', 'Lion', 'Virgin', 'Balance', 'Scorpion', 'Archer', 'Goat', 'Water Bearer', 'Fish'];
 
-const pics = [];
+const pics = ['./resources/images/bruce_lee.jpeg', './resources/images/jason-statham.jpeg', './resources/images/mario.jpeg', './resources/images/neighbor.jpeg', './resources/images/nicole-kidman.jpeg', './resources/images/pugacheva.jpeg'];
 
 function randomizer (arr) {
     let choice = arr[Math.floor(Math.random() * arr.length)]; 
     return choice;
 }
 
+function cleanParagraph() {
+    let elementToRemove = document.getElementsByTagName('p')[0];
+    document.getElementById('quote').removeChild(elementToRemove);
+}
+
+function cleanPhoto () {
+    let elementToRemove1 = document.getElementsByTagName('img')[0];
+    let elementToRemove2 = document.getElementsByTagName('caption')[0];
+
+    document.getElementById('pic').removeChild(elementToRemove1);
+    document.getElementById('pic').removeChild(elementToRemove2);
+}
+
+function setter () {
+    
+    if (document.getElementsByTagName('p').length === 1) {
+        cleanParagraph()
+    }
+    if (document.getElementsByTagName('img').length === 1) {
+        cleanPhoto()
+    }
+
+    const phrase = randomizer(phrases);
+    const zodiac = randomizer(zodiacs);
+    const pic = randomizer(pics);
+    const author = randomizer(authors);
+
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = ('If your zodiac is ' + zodiac + ', then mark these words: </br></br></br>' + phrase);
+    document.getElementById('quote').appendChild(paragraph);
+    
+    let photo = document.createElement('img');
+    photo.src = pic;
+    document.getElementById('pic').appendChild(photo);
+
+    let caption = document.createElement('caption');
+    caption.innerHTML = ('Author: ' + author); 
+    document.getElementById('pic').appendChild(caption);
+
+    document.getElementById('pic').style.padding = '1rem';
+    document.getElementsByClassName('container')[0].style.padding = '2rem';
+}
+
+let button = document.querySelector('button').onclick = setter;
