f69b669aac6c12e690141d6d6d0f7c950cce68fe
Denis Sigulin <118192424+Sigulek@users.noreply.github.com>
2022-12-07 00:35:52 +0400

    Add items to arrays

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,26 @@
-const authors = ['A.Pugacheva', 'J.Statham', 'N.Kidman', 'B.Lee', 'Unknown author', 'Your neighbor'];
-const phrases = ['Be yourself; everyone else is already taken.', 
+const authors = ['A.Pugacheva', 'J.Statham', 'N.Kidman', 'B.Lee', 'Unknown author', 'Your neighbor', 'The One', 'J.Lennon'];
+const phrases = [
+    'Be yourself; everyone else is already taken.', 
 "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
 "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
 "So many books, so little time.",
 "A friend is someone who knows all about you and still loves you.",
 "To live is the rarest thing in the world. Most people exist, that is all.",
 "I am so clever that sometimes I don't understand a single word of what I am saying.",
+"Life is never fair, and perhaps it is a good thing for most of us that it is not.",
+"Life is what happens when you're busy making other plans."
 ];
 const zodiacs = ['Ram', 'Bull', 'Twins', 'Crab', 'Lion', 'Virgin', 'Balance', 'Scorpion', 'Archer', 'Goat', 'Water Bearer', 'Fish'];
 
-const pics = ['./resources/images/bruce_lee.jpeg', './resources/images/jason-statham.jpeg', './resources/images/mario.jpeg', './resources/images/neighbor.jpeg', './resources/images/nicole-kidman.jpeg', './resources/images/pugacheva.jpeg'];
+const pics = ['./resources/images/bruce_lee.jpeg', 
+'./resources/images/jason-statham.jpeg', 
+'./resources/images/mario.jpeg', 
+'./resources/images/neighbor.jpeg', 
+'./resources/images/nicole-kidman.jpeg', 
+'./resources/images/pugacheva.jpeg', 
+'./resources/images/lennon.jpg', 
+'./resources/images/neo.jpg'
+];
 
 function randomizer (arr) {
     let choice = arr[Math.floor(Math.random() * arr.length)]; 
@@ -44,7 +55,7 @@ function setter () {
     const author = randomizer(authors);
 
     let paragraph = document.createElement('p');
-    paragraph.innerHTML = ('If your zodiac is ' + zodiac + ', then mark these words: </br></br></br>' + phrase);
+    paragraph.innerHTML = ('If your zodiac is ' + zodiac + ', then mark these words: </br></br></br>' + '"' + phrase + '"');
     document.getElementById('quote').appendChild(paragraph);
     
     let photo = document.createElement('img');
